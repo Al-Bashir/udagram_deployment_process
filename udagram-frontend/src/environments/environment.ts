6b6f03add0ddc503029bc39504855be2f4e75c13
Al-Bashir <mohamedg1995@yahoo.com>
2022-05-15 23:00:12 +0200

    trial no. 9

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api.eba-zjtynyr9.us-east-1.elasticbeanstalk.com/api/v0/'
+  apiHost: 'http://udagram-api.eba-9ytmfeqh.us-east-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*
